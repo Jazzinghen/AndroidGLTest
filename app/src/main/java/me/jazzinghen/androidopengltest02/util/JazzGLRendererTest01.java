7ec0f2f4fdcd49851cea6ea6f738f1a2d09dd047
Michele Jazzinghen Bianchi <jazzinghen@gmail.com>
2015-02-19 14:45:33 +0100

    Completed implementation skeleton for test for first OpenGLES lesson

--- app/src/main/java/me/jazzinghen/androidopengltest02/util/JazzGLRendererTest01.java
+++ app/src/main/java/me/jazzinghen/androidopengltest02/util/JazzGLRendererTest01.java
@@ -1,7 +1,29 @@
 package me.jazzinghen.androidopengltest02.util;
 
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 /**
  * Created by jazzinghen on 19/02/15.
  */
-public class JazzGLRendererTest01 {
+public class JazzGLRendererTest01 implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // Redraw background color
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+    }
 }
